5cb8713b0289545fff6383ca33c97ee409240bd6
Caven <cavenasdev@gmail.com>
2024-07-15 14:16:53 +0800

    feat: add projects

--- uno.config.ts
+++ uno.config.ts
@@ -5,14 +5,16 @@ export default defineConfig({
   presets: [presetUno(), presetIcons({})],
   theme: {
     fontFamily: {
-      display:
-        '"Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif',
+      display: 'Roboto, system-ui, -apple-system, "Segoe UI", "Helvetica Neue", Arial, "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      // '"Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif',
       vr: "varela_round",
     },
     colors: {
       sun: "#FEC74A",
       sea: "#62A3FF",
       forest: "#32D583",
+      brand: '#FFD268',
+      cvTextPrimary: "#0E121B"
     },
     boxShadow: {
       4: "4px 4px #0f172a",
